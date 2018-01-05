597354c78f5f413a7ca9b7fa50b9a8c4e0f11f22
Ting-Hao Chen <email.chentinghao@gmail.com>
2018-01-05 17:21:27 +0800

    Update download_gdrive.py

--- download_gdrive.py
+++ download_gdrive.py
@@ -4,14 +4,6 @@ In your terminal, run the command:
 
 python download_gdrive.py GoogleFileID /path/for/this/file/to/download/file.type
 
-Example:
-GoogleFileID: (only includes the characters of the id)
-https://drive.google.com/uc?export=download&confirm=1o_3&id=0Bz7KyqmuGsilT0J5dmRCM0ROVHc
-/path/for/this/file/to/download/file.type:
-/home/ubuntu/myfile/file.zip
-
-python download_gdrive.py 0Bz7KyqmuGsilT0J5dmRCM0ROVHc /home/ubuntu/myfile/file.zip
-
 Credited to 
 https://stackoverflow.com/questions/25010369/wget-curl-large-file-from-google-drive
 author: https://stackoverflow.com/users/1475331/user115202
